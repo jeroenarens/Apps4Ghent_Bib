7df26e0657265f6fa2de00b79fbd76bdcf828ea5
immortaleeb <immortaleeb@gmail.com>
2015-03-07 10:04:38 +0100

    Add model for library items

--- Apps4Ghent_Library/apps4ghent/models.py
+++ Apps4Ghent_Library/apps4ghent/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from mongoengine import Document, StringField, IntField, BooleanField
 
-# Create your models here.
+# TODO: put blank constraints on the fields of models as soon as we get some example data 
+class Item(Document):
+    """Represents an item present in the library (e.g. a book),
+    storing mostly its description"""
+    
+    # General information (can be used "outside library context")
+    BB_number = StringField(max_length=45)
+    title = StringField(max_length=45)
+    ISBN = StringField(max_length=45)
+    ISSN = StringField(max_length=45)
+    material = StringField(max_length=45)
+    visible_in_webopace = BooleanField()
+    author_and_functions = StringField(max_length=45)
+
+    # Information that can only be used "inside library context"
+    number_of_pages = IntField()
+    keywords = StringField(max_length=45)
+    genre = StringField(max_length=45)
+    theme = StringField(max_length=45)
+    language = StringField(max_length=45)
+    translated = StringField(max_length=1)
+    literary_nature = StringField(max_length=45)
+    edition = StringField(max_length=45)
+    impressum = StringField(max_length=45)
+    SISO = StringField(max_length=45)
+    ZIZO = StringField(max_length=45)
+    age_description = StringField(max_length=45)
+    AVI = StringField(max_length=45)
+    complete_title = StringField(max_length=45)
