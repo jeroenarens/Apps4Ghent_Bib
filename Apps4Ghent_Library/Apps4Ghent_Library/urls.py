93743a5c8c8d962b577445c0e88e76099cc0e986
immortaleeb <immortaleeb@gmail.com>
2015-03-15 13:16:55 +0100

    Add prefix api/v1/ for api URLs

--- Apps4Ghent_Library/Apps4Ghent_Library/urls.py
+++ Apps4Ghent_Library/Apps4Ghent_Library/urls.py
@@ -4,12 +4,17 @@ from django.contrib import admin
 from apps4ghent import views
 
 urlpatterns = patterns('',
+    # General urls
     url(r'^$', views.index, name='index'),
+    
+    # Admin section
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^items/$', views.ItemList.as_view()),
-    url(r'^item-copies/$', views.ItemCopyList.as_view()),
-    url(r'^borrowers/$', views.BorrowerList.as_view()),
-    url(r'^borrowings/$', views.BorrowingList.as_view()),
-    url(r'^reservations/$', views.ReservationList.as_view()),
+    
+    # Api section
+    url(r'^api/v1/items/$', views.ItemList.as_view()),
+    url(r'^api/v1/item-copies/$', views.ItemCopyList.as_view()),
+    url(r'^api/v1/borrowers/$', views.BorrowerList.as_view()),
+    url(r'^api/v1/borrowings/$', views.BorrowingList.as_view()),
+    url(r'^api/v1/reservations/$', views.ReservationList.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
