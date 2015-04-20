82efc0f5151c23fa59fcf9ad2c2edfb5519936c7
immortaleeb <immortaleeb@gmail.com>
2015-04-20 13:59:01 +0200

    Add router to urls

--- Apps4Ghent_Library/Apps4Ghent_Library/urls.py
+++ Apps4Ghent_Library/Apps4Ghent_Library/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
 
 from apps4ghent import views
 
 # Register resources
+router = routers.SimpleRouter()
+router.register(r'items', views.ItemViewSet)
 
 urlpatterns = patterns('',
     # General urls
@@ -17,5 +20,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     # Api section
+    url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
