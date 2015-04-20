0a87111a6e1c84018d0a58f67fa0a23bbd41dd5e
immortaleeb <immortaleeb@gmail.com>
2015-04-20 13:16:05 +0200

    Remove all mongodb stuff

--- Apps4Ghent_Library/Apps4Ghent_Library/urls.py
+++ Apps4Ghent_Library/Apps4Ghent_Library/urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from rest_framework_mongoengine.routers import MongoDefaultRouter
 
 from apps4ghent import views
 
 # Register resources
-router = MongoDefaultRouter()
-router.register(r'items', views.ItemsViewSet)
-router.register(r'item-copies', views.ItemCopyViewSet)
-router.register(r'borrowers', views.BorrowerViewSet)
-router.register(r'borrowings', views.BorrowingViewSet)
-router.register(r'reservations', views.ReservationViewSet)
 
 urlpatterns = patterns('',
     # General urls
@@ -24,9 +17,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     # Api section
-    url(r'^api/v1/borrowed-items/borrowings/count', views.BorrowedItemsBorrowingsCountView.as_view()),
-    url(r'^api/v1/borrowed-items/borrowings', views.BorrowedItemsBorrowingsView.as_view()),
-    url(r'^api/v1/borrowed-items', views.BorrowedItemsView.as_view()),
-    url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
