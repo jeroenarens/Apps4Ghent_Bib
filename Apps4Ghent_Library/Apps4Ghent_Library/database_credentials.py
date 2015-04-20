1cfb142029e30e97acd5b0cd3878f232b6eed271
immortaleeb <immortaleeb@gmail.com>
2015-04-20 13:03:22 +0200

    Remove references to mongo in settings files
    Also provides credentials to connect to a postgres database
    and added the psycogp driver necessary to connect with postgres to the
    requirements file.

--- Apps4Ghent_Library/Apps4Ghent_Library/database_credentials.py
+++ Apps4Ghent_Library/Apps4Ghent_Library/database_credentials.py
@@ -1,11 +1,6 @@
-# Database credentials
-_MONGODB_USER = 'mongouser'
-_MONGODB_PASSWD = 'password'
-_MONGODB_HOST = 'localhost'
-_MONGODB_NAME = 'apps4ghent'
+# Change database credentials here
+_POSTGRES_USER = 'library'
+_POSTGRES_PASSWD = ''
+_POSTGRES_HOST = 'localhost'
+_POSTGRES_NAME = 'postgres'
 
-# Connection string without any authentication
-_MONGODB_DATABASE_HOST = 'mongodb://%s/%s' % (_MONGODB_HOST, _MONGODB_NAME)
-
-# Connection string with user/pass authentication
-#_MONGODB_DATABASE_HOST = 'mongodb://%s:%s@%s/%s' % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
