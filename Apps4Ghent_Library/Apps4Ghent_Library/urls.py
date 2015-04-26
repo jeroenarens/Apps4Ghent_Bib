adf3522822bf9d74f4a6d737cdb8e24255a4e04b
= <=>
2015-04-26 18:51:57 +0200

    test

--- Apps4Ghent_Library/Apps4Ghent_Library/urls.py
+++ Apps4Ghent_Library/Apps4Ghent_Library/urls.py
@@ -14,11 +14,11 @@ router.register(r'reservations', views.ReservationViewSet)
 
 urlpatterns = patterns('',
     # General urls
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.openlayers, name='openlayers'),
     url(r'^overview/', views.overview, name='overview'),
     url(r'^leaflet/', views.leaflet, name='leaflet'),
     url(r'^highmap/', views.highmap, name='highmap'),
-    url(r'^openlayers/', views.openlayers, name='openlayers'),
+    url(r'^openlayers/', views.index, name='index'),
     
     # Admin section
     url(r'^admin/', include(admin.site.urls)),
