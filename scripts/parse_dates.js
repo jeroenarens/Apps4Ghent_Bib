f019a68dd4e8ebdf53bbcd87e657f1e40db36844
immortaleeb <immortaleeb@gmail.com>
2015-05-11 13:51:01 +0200

    Remove old scripts

--- scripts/parse_dates.js
+++
@@ -1,8 +0,0 @@
-var cursor = db.borrowing.find();
-while (cursor.hasNext()) {
-  var borrowing = cursor.next();
-  var split = borrowing.from_date.split('/').map(function(el) { return parseInt(el); });
-  var new_date = new Date(split[2], split[1]-1, split[0]);
-
-  db.borrowing.update({_id: borrowing._id}, {$set: {from_date: new_date}});
-}
