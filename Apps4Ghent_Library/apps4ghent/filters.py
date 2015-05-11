34a5db1358e00b49cff9d80badc604320874302d
immortaleeb <immortaleeb@gmail.com>
2015-05-11 19:49:45 +0200

    Add type, decade and sex filter to borrowed items

--- Apps4Ghent_Library/apps4ghent/filters.py
+++ Apps4Ghent_Library/apps4ghent/filters.py
@@ -5,8 +5,11 @@ from .models import *
 class BorrowedItemFilter(django_filters.FilterSet):
     from_library = django_filters.CharFilter(name='item_copy__location')
     to_sector = django_filters.NumberFilter(name='borrower__sector_id')
+    type = django_filters.CharFilter(name='item_copy__item__type')
+    decade = django_filters.NumberFilter(name='borrower__decade')
+    sex = django_filters.CharFilter(name='borrower__sex')
 
     class Meta:
         model = Borrowing
-        fields = ['from_library', 'to_sector']
+        fields = ['from_library', 'to_sector', 'type', 'decade', 'sex']
 
