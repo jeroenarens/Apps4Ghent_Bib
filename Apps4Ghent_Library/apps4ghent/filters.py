ecf7f5b32a14a23a757278335a5ba2877f5ae2a7
immortaleeb <immortaleeb@gmail.com>
2015-05-11 20:30:40 +0200

    Add filters for borrowing dates

--- Apps4Ghent_Library/apps4ghent/filters.py
+++ Apps4Ghent_Library/apps4ghent/filters.py
@@ -8,8 +8,10 @@ class BorrowedItemFilter(django_filters.FilterSet):
     type = django_filters.CharFilter(name='item_copy__item__type')
     decade = django_filters.NumberFilter(name='borrower__decade')
     sex = django_filters.CharFilter(name='borrower__sex')
+    from_date = django_filters.DateFilter(name='from_date', lookup_type='gte')
+    until_date = django_filters.DateFilter(name='from_date', lookup_type='lte')
 
     class Meta:
         model = Borrowing
-        fields = ['from_library', 'to_sector', 'type', 'decade', 'sex']
+        fields = ['from_library', 'to_sector', 'type', 'decade', 'sex', 'from_date', 'until_date']
 
