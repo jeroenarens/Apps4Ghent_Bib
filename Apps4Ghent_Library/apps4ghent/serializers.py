6739ca8f550aba63ec396b779b012828d9984a45
immortaleeb <immortaleeb@gmail.com>
2015-05-13 00:48:23 +0200

    Remove duplicate serializer

--- Apps4Ghent_Library/apps4ghent/serializers.py
+++ Apps4Ghent_Library/apps4ghent/serializers.py
@@ -53,7 +53,3 @@ class BorrowedItemSerializer(serializers.Serializer):
     from_library = serializers.CharField(source='item_copy__location')
     to_sector = serializers.IntegerField(source='borrower__sector_id')
     borrowing_count = serializers.IntegerField(source='bcount')
-
-#Here the amount of books of a borrower is serialized
-class BorrowerCountSerializer(serializers.Serializer):
-    borrower_count = serializers.IntegerField(source='bcount')
